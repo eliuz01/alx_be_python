c46658fc2b607568916496f7a0340a5a73a6f238
ELIUZ <osongoeliuz14@gmail.com>
2024-12-18 11:08:24 +0300

    created match_case calc

---
+++ match_case_calculator.py
@@ -0,0 +1,24 @@
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+case_operation  = input("choose the operation (/,*,+,-: ): ")
+
+match case_operation: 
+    case "+":
+        result = num1 + num2
+        print(f"the result is {result}")
+    case "/":
+        if num2 != 0:
+            result = num1/num2
+            print(f"the result is {result}")
+        else:
+            print("Error: Cannot divide by zero.")
+    case "*":
+        result = num1 * num2
+        print(f"the result is {result}")
+    case "-":
+        result = num1 - num2
+        print(f"the result is {result}")
+    case _:
+        print("Invalid operation selected. Please choose from (+, -, *, /).")
+
