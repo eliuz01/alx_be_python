78b8d182d26d437615e4719e22b8d77aaaf51611
ELIUZ <osongoeliuz14@gmail.com>
2025-01-19 07:32:22 +0300

    final program

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.account_balance = account_balance
+    
+    def deposit(self, amount):
+        if amount> 0:
+            self.account_balance = self.account_balance + amount
+        else:
+            print("You cannot deposit zero amount")
+    def withdraw(self, amount):
+        """Withdraw a certain amount from the account, if sufficient funds are available."""
+        if amount > 0:
+            if self.account_balance >= amount:
+                self.account_balance -= amount
+                return True
+            else:
+                return False
+        else:
+            print("Withdrawal amount must be positive.")
+            return False
+    def display_balance(self):
+        print(f"Your account balance is {self.account_balance:.2f}")
+
+
